316943e7a4833f74acf007feff357389bd95c5f2
xzsaxxz <147250871+xzsaxxz@users.noreply.github.com>
2023-12-04 20:07:11 +0300

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 import os
 import re
 import textwrap
@@ -7,10 +17,8 @@ import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from YukkiMusic import app
 
-from config import YOUTUBE_IMG_URL,MUSIC_BOT_NAME
-
+from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -18,40 +26,12 @@ def changeImageSize(maxWidth, maxHeight, image):
     heightRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
-    return newImage
+    return image.resize((newWidth, newHeight))
 
-def circle(img):
-    h,w=img.size
-    a = Image.new('L', [h,w], 0)
-    b = ImageDraw.Draw(a)
-    b.pieslice([(0, 0), (h,w)], 0, 360, fill = 255,outline = "white")
-    c = np.array(img)
-    d = np.array(a)
-    e = np.dstack((c, d))
-    return Image.fromarray(e)
 
-def circle2(img,Xcenter,Ycenter):
-    h,w=img.size
-    a = Image.new('L', [h,w], 0)
-    b = ImageDraw.Draw(a)
-    b.pieslice([(0, 0), (h,w)], 0, 360, fill = 255,outline = "white")
-    b.pieslice([(int(Xcenter-50), int(Ycenter-30)), (int(Xcenter+50),int(Ycenter+30))], 0, 360, fill = 0,outline = "black")
-    c = np.array(img)
-    d = np.array(a)
-    e = np.dstack((c, d))
-    return Image.fromarray(e)
-
-def squ(img1):
-    a = Image.new('L', [640,500], 0)
-    b=ImageDraw.Draw(a)
-    b.line((320,0,240,550),fill="white",width=670)
-    e=Image.fromarray(np.dstack((np.array(img1),np.array(a))))
-    return e
-
-async def gen_thumb(videoid,user_id):
-    if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
-       return f"cache/{videoid}_{user_id}.png"
+async def gen_thumb(videoid):
+    if os.path.isfile(f"cache/{videoid}.png"):
+        return f"cache/{videoid}.png"
 
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
@@ -59,14 +39,14 @@ async def gen_thumb(videoid,user_id):
         for result in (await results.next())["result"]:
             try:
                 title = result["title"]
-            
+                title = re.sub("\W+", " ", title)
                 title = title.title()
             except:
                 title = "Unsupported Title"
             try:
                 duration = result["duration"]
             except:
-                   duration = "Unknown Mins"
+                duration = "Unknown Mins"
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
             try:
                 views = result["viewCount"]["short"]
@@ -76,6 +56,7 @@ async def gen_thumb(videoid,user_id):
                 channel = result["channel"]["name"]
             except:
                 channel = "Unknown Channel"
+
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
@@ -84,24 +65,7 @@ async def gen_thumb(videoid,user_id):
                     )
                     await f.write(await resp.read())
                     await f.close()
-        try: 
-            async for photo in app.get_chat_photos(user_id,1): 
-                sp=await app.download_media(photo.file_id, file_name=f'{user_id}.jpg') 
-        except: 
-            async for photo in app.get_chat_photos(app.id,1): 
-                sp=await app.download_media(photo.file_id, file_name=f'{app.id}.jpg')
-        xp=Image.open(sp)
-        youtube = Image.open(f"cache/thumb{videoid}.png")
-        image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(70))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.5)
-        Xcen1 = image1.width / 2
-        Ycen1 = image1.height / 2
-        Xcen2 =xp.width / 2
-        Ycen2 = xp.height / 2
-        
+
         youtube = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
@@ -208,4 +172,3 @@ async def gen_thumb(videoid,user_id):
         return f"cache/{videoid}_{user_id}.png"
     except Exception as e:
         return YOUTUBE_IMG_URL
-
