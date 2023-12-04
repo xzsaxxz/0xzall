d8be0091a3cc9702302149b7104956c200a50a53
xzsaxxz <147250871+xzsaxxz@users.noreply.github.com>
2023-12-04 20:15:49 +0300

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -39,7 +39,7 @@ async def gen_thumb(videoid):
         for result in (await results.next())["result"]:
             try:
                 title = result["title"]
-                title = re.sub("\W+", " ", title)
+                
                 title = title.title()
             except:
                 title = "Unsupported Title"
